901d49c7348466252a3f11ac92ffa3f0901b3661
Arthur <arthur.hordiienko@gmail.com>
2023-10-25 22:45:42 +0300

    created new container web, component table, & general layout basic

--- src/route/index.js
+++ src/route/index.js
@@ -49,10 +49,7 @@ router.get('/', function (req, res) {
 
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/summary', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
-    // ↙ сюди вводимо JSON дані
-
     page: {
       title: 'Resume | Summary',
     },
@@ -81,10 +78,7 @@ router.get('/summary', function (req, res) {
 })
 
 router.get('/skills', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('skills', {
-    // ↙ сюди вводимо JSON дані
-
     page: {
       title: 'Resume | Skills',
     },
@@ -141,10 +135,7 @@ router.get('/skills', function (req, res) {
 })
 
 router.get('/education', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('education', {
-    // ↙ сюди вводимо JSON дані
-
     page: {
       title: 'Resume | Education',
     },
@@ -183,9 +174,7 @@ router.get('/education', function (req, res) {
 })
 
 router.get('/work', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('work', {
-    // ↙ сюди вводимо JSON дані
     layout: 'big',
     page: {
       title: 'Resume | Work',
@@ -244,9 +233,8 @@ router.get('/work', function (req, res) {
 })
 
 router.get('/person', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('person', {
-    layout: 'person',
+    layout: 'basic',
     page: {
       title: 'Resume | Person',
     },
@@ -348,7 +336,7 @@ router.get('/person', function (req, res) {
 router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('bio', {
-    layout: 'bio',
+    layout: 'basic',
     page: {
       title: 'Resume | Biography',
     },
@@ -445,9 +433,8 @@ router.get('/bio', function (req, res) {
 })
 
 router.get('/program', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
   res.render('program', {
-    layout: 'program',
+    layout: 'basic',
     page: {
       title: 'Resume | Program',
     },
@@ -527,6 +514,136 @@ router.get('/program', function (req, res) {
           },
         ],
       },
+    },
+  })
+})
+
+router.get('/web', function (req, res) {
+  res.render('web', {
+    layout: 'basic',
+    page: {
+      title: 'Resume | Web',
+    },
+    web: {
+      languages: [
+        {
+          name: 'HTML',
+          version: 'HTML5',
+          description:
+            'Hypertext Markup Language is the standard markup language for creating web pages and web applications.',
+          elements: [
+            {
+              name: 'div',
+              description:
+                'Defines a division or a section in an HTML document.',
+              attributes: [
+                {
+                  value: 'id',
+                  description:
+                    'Specifies a unique id for an HTML element.',
+                },
+                {
+                  value: 'class',
+                  description:
+                    'Specifies one or more class names for an HTML element.',
+                },
+              ],
+            },
+            {
+              name: 'p',
+              description:
+                'Defines a paragraph in an HTML document.',
+              attributes: [
+                {
+                  value: 'id',
+                  description:
+                    'Specifies a unique id for an HTML element.',
+                },
+                {
+                  value: 'class',
+                  description:
+                    'Specifies one or more class names for an HTML element.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'CSS',
+          version: 'CSS3',
+          description:
+            'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in HTML or XML.',
+          properties: [
+            {
+              name: 'color',
+              description: 'Sets the color of the text.',
+              values: [
+                {
+                  value: 'red',
+                  description:
+                    'Sets the text color to red.',
+                },
+                {
+                  value: 'blue',
+                  description:
+                    'Sets the text color to blue.',
+                },
+              ],
+            },
+            {
+              name: 'background-color',
+              description:
+                'Sets the background color of an element.',
+              values: [
+                {
+                  value: 'white',
+                  description:
+                    'Sets the background color to white.',
+                },
+                {
+                  value: 'black',
+                  description:
+                    'Sets the background color to black.',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'JavaScript',
+          version: 'ES6',
+          description:
+            'JavaScript is a programming language used to create interactive effects within web browsers.',
+          functions: [
+            {
+              name: 'alert()',
+              description:
+                'Displays an alert box with a specified message and an OK button.',
+              parameters: [
+                {
+                  name: 'message',
+                  type: 'string',
+                  description:
+                    'The message to display in the alert box.',
+                },
+              ],
+            },
+            {
+              name: 'getElementById()',
+              description:
+                'Returns the element with the specified ID.',
+              parameters: [
+                {
+                  name: 'id',
+                  type: 'string',
+                  description:
+                    'The ID of the element to find.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
     },
   })
 })
